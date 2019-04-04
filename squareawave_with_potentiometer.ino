eaea5a4de37ab656adad9c19be21ca5d73b7b569
Max Goad <maxdgoad@gmail.com>
2019-04-04 14:21:35 -0500

    added squarewave + frequency demo file
    This file expands upon the previous example by implementing an input device(potentiometer) and using its value (which is then mapped to a more useful range of values) to change the delay in between pulses. This will change the frequency of the wave which the aux outputs. (essentially, pulse width modulation (PWM))

---
+++ squareawave_with_potentiometer.ino
@@ -0,0 +1,26 @@
+/*
+ * KU EECS 388 project
+ * This is the main for the synth
+ * blah blah blah 
+ */
+ // square wave with frequency controlled by potentiometer
+
+const int ledPin = 9;  
+const int potPin = A0; 
+int value; 
+
+
+void setup(){
+  pinMode(ledPin, OUTPUT); 
+  pinMode(potPin, INPUT);
+}
+
+void loop(){
+  
+  value = analogRead(potPin);          //Read and save analog value from potentiometer
+  value = map(value, 0, 1023, 10000, 2000); //Map value 0-1023 to 10000-2000 (delay in usec)
+  digitalWrite(ledPin, HIGH);          
+  delayMicroseconds(value); // delay is now dependent on the potentiometer position.
+  digitalWrite(ledPin, LOW);
+  delayMicroseconds(value);
+}
